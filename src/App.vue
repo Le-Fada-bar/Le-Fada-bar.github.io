a1bc624842bdefb121b9f8176304250cf1a8cda5
@RobinCamarasa <robin.camarasa@gmail.com>
2024-04-10 12:28:21 +0200

    Add burger menu for mobile

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,69 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      toggled: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.toggled = !this.toggled;
+    },
+    close() {
+      this.toggled = false;
+    }
+  },
+  mounted() {
+  },
+  unmounted() {
+  },
+}
+
 </script>
 
 <template>
   <header>
     <nav>
-      <RouterLink to="/">Fada</RouterLink>
-      <RouterLink to="/contact">Contact</RouterLink>
-      <RouterLink to="/events">Evènements</RouterLink>
-      <RouterLink to="/menu">Ardoise</RouterLink>
-      <RouterLink to="/information">Informations</RouterLink>
+      <div class="fada">
+        <RouterLink to="/" @click="close">Fada</RouterLink>
+        <div><button @click="toggle" class="burger-menu">=</button></div>
+      </div>
+      <div>
+        <RouterLink v-if="toggled" @click="close" to="/contact">Contact</RouterLink>
+      </div>
+      <div>
+        <RouterLink v-if="toggled" @click="close" to="/events">Evènements</RouterLink>
+      </div>
+      <div>
+        <RouterLink v-if="toggled" @click="close" to="/menu">Ardoise</RouterLink>
+      </div>
+      <div>
+        <RouterLink v-if="toggled" @click="close" to="/information">Informations</RouterLink>
+      </div>
     </nav>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  position: sticky;
+  border-bottom: solid;
+}
+
+.fada {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.burger-menu {
+  position: sticky;
+  min-height: 1.5em;
+  aspect-ratio: 1;
+}
+</style>
