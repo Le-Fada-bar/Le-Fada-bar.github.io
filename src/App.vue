b58770241cfe7a0bd2dc5b71be994d1ea7d7c949
@RobinCamarasa <robin.camarasa@gmail.com>
2024-04-11 19:30:59 +0200

    Add adaptive colors for menu

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,8 @@ export default {
       toggled: false,
       menu_height: "2em",
       menu_inside: "=",
+      menu_background_color: "rgba(0, 0, 0, 0)",
+      menu_color: "var(--theme-white)",
     }
   },
   methods: {
@@ -25,29 +27,52 @@ export default {
       this.toggled = true;
       this.menu_height = "100vh";
       this.menu_inside = "x";
+      this.colored_menu();
     },
     close() {
       this.toggled = false;
       this.menu_height = "2em";
       this.menu_inside = "=";
+      this.onScroll()
+    },
+    transparent_menu() {
+      this.menu_background_color = "rgba(0, 0, 0, 0)";
+      this.menu_color = "#ffffff";
+    },
+    colored_menu() {
+      this.menu_background_color = "var(--theme-white)";
+      this.menu_color = "var(--theme-black)";
+    },
+    onScroll() {
+      if (this.$route.name == "home" && window.scrollY < (.5 * window.innerHeight)) {
+        this.transparent_menu();
+      } else {
+        this.colored_menu();
+      }
     }
   },
   mounted() {
+    window.addEventListener('scroll', this.onScroll);
   },
   unmounted() {
+    window.removeEventListener('scroll', this.onScroll);
   },
+  watch: {
+    $route(to, from) {
+      this.onScroll();
+    }
+  }
 }
 
 </script>
 
 <template>
-  <header :style="'height: ' + menu_height">
+  <header
+    :style="'height: ' + menu_height + '; background-color: ' + menu_background_color + '; color: ' + menu_color + ';'">
+    <div class="burger-div"><button @click="toggle" class="burger-menu">{{ menu_inside }}</button></div>
     <nav>
-      <div class="fada">
-        <div>
-          <RouterLink to="/" @click="close">Fada</RouterLink>
-        </div>
-        <div><button @click="toggle" class="burger-menu">{{ menu_inside }}</button></div>
+      <div>
+        <RouterLink to="/" @click="close">Fada</RouterLink>
       </div>
       <div>
         <a href="#" v-if="toggled" @click="close">Réservez</a>
@@ -59,15 +84,17 @@ export default {
         <RouterLink v-if="toggled" @click="close" to="/events">Evènements</RouterLink>
       </div>
       <div>
+        <RouterLink v-if="toggled" @click="close" to="/contact">Contactez-nous</RouterLink>
       </div>
-      <RouterLink v-if="toggled" @click="close" to="/contact">Contactez-nous</RouterLink>
       <div>
         <RouterLink v-if="toggled" @click="close" to="/information">Informations</RouterLink>
       </div>
-      <Footer v-if="toggled" />
     </nav>
+    <Footer v-if="toggled" />
   </header>
-  <RouterView style="min-height: 80vh" />
+  <div class="container">
+    <RouterView style="min-height: 80vh" />
+  </div>
   <Footer />
 </template>
 
@@ -77,17 +104,31 @@ header {
   position: fixed;
   border-bottom: solid;
   width: 100%;
-  background-color: var(--theme-white);
+  /* background-color: var(--theme-white); */
 }
 
-.fada {
+nav {
   display: flex;
+  width: 100%;
+  flex-direction: column;
   align-items: center;
   justify-content: space-between;
 }
 
+nav>div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: var(--theme-spacing);
+}
+
+.burger-div {
+  top: 0;
+  position: fixed;
+}
+
 .burger-menu {
-  min-height: 1.5em;
+  height: 2em;
   aspect-ratio: 1;
 }
 </style>
