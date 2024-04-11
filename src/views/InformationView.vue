613b79dfb875491507e06854dc0040ef893fdce3
@RobinCamarasa <robin.camarasa@gmail.com>
2024-04-11 11:21:54 +0200

    Add minimal information vue

--- src/views/InformationView.vue
+++ src/views/InformationView.vue
@@ -1,9 +1,25 @@
-<script></script>
+<script>
+import PictureText from '../components/PictureText.vue'
+
+export default {
+  components: { PictureText }
+}
+</script>
 
 <template>
-  <div>
-    <h1>This is the Information vue</h1>
-  </div>
+  <PictureText>
+    <h2>Horaires</h2>
+    <ul>
+      <li>#DATE</li>
+      <li>#DESCRIPTION</li>
+    </ul>
+  </PictureText>
+  <PictureText>
+    <h2>Où nous trouver</h2>
+    <ul>
+      <li>#Addresse</li>
+    </ul>
+  </PictureText>
 </template>
 
 <style></style>
