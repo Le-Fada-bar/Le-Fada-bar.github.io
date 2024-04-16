5813f7c6b5fb6c8f9b40ed4ccfe4ec5c170e6efe
@RobinCamarasa <robin.camarasa@gmail.com>
2024-04-16 11:51:25 +0200

    Add LogoLink to contact view

---
+++ src/components/LogoLink.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  props: {
+    displayed: {
+      type: String
+    },
+    address: {
+      type: String
+    },
+    logo: {
+      type: String,
+      default: "maps"
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="logo"><a :href="address"><img style="height: 1em; margin-right: 0.5em"
+        :src="'/icons/' + logo + '_white.svg'" alt="">
+      <slot></slot>
+    </a></div>
+
+</template>
+
+<style scoped>
+.logo {
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+}
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,8 +1,9 @@
 <script>
 import PictureText from '../components/PictureText.vue'
+import LogoLink from '../components/LogoLink.vue'
 
 export default {
-  components: { PictureText }
+  components: { PictureText, LogoLink }
 }
 </script>
 
@@ -10,11 +11,10 @@ export default {
   <main>
     <PictureText picture="barmen.jpg">
       <h2>CONTACTEZ-NOUS</h2>
-      <ul>
-        <li> Address</li>
-        <li> Phone</li>
-        <li> Mail</li>
-      </ul>
+      <LogoLink address="telto:+33612345678" logo="phone">+33 6 12 34 56 78
+      </LogoLink>
+      <LogoLink address="https://maps.app.goo.gl/eBKBerbVwWXnuEybA" logo="maps">64 Rue Jean-Jacques Rousseau</LogoLink>
+      <LogoLink address="#" logo="mail">fada.bar@gmail.com</LogoLink>
     </PictureText>
   </main>
 </template>
